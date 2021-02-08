d0d3f4fa8f8c4498dd8a53692836eb5b738c80d0
faustotrindadejr@gmail.com <faustotrindadejr@gmail.com>
2021-02-07 21:21:44 -0300

    commit 3 mapeamento das entidades

--- src/main/java/com/fausto/bookstore/domain/Livro.java
+++ src/main/java/com/fausto/bookstore/domain/Livro.java
@@ -1,10 +1,28 @@
 package com.fausto.bookstore.domain;
 
-public class Livro {
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Livro implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
 	private String texto;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_categoria")
 	private Categoria categoria;
 	
 	public Livro() {
