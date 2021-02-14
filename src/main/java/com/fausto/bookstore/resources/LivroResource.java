5827414563e1c126fecdd0ff96b1d6555dbcdd5d
faustotrindadejr@gmail.com <faustotrindadejr@gmail.com>
2021-02-14 01:11:06 -0300

    Metodo findById da entidade Livro

---
+++ src/main/java/com/fausto/bookstore/resources/LivroResource.java
@@ -0,0 +1,26 @@
+package com.fausto.bookstore.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fausto.bookstore.domain.Livro;
+import com.fausto.bookstore.service.LivroService;
+
+@RestController
+@RequestMapping(value = "/livros")
+public class LivroResource {
+	
+	@Autowired
+	private LivroService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Livro> findById(@PathVariable Integer id){
+		Livro obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/fausto/bookstore/service/LivroService.java
@@ -0,0 +1,23 @@
+package com.fausto.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fausto.bookstore.domain.Livro;
+import com.fausto.bookstore.exceptions.ObjectNotFoundException;
+import com.fausto.bookstore.repositories.LivroRepository;
+
+@Service
+public class LivroService {
+
+	@Autowired
+	private LivroRepository repository;
+	
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado  Id: "+ id +" , Tipo: "+Livro.class.getName()));
+	}
+}
