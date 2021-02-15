f277676888a52ef3b55b52601885444d582a47a8
faustotrindadejr@gmail.com <faustotrindadejr@gmail.com>
2021-02-14 21:50:32 -0300

    Update livro utilizando os tipos PUT e PATCH

--- src/main/java/com/fausto/bookstore/resources/LivroResource.java
+++ src/main/java/com/fausto/bookstore/resources/LivroResource.java
@@ -6,12 +6,14 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fausto.bookstore.domain.Categoria;
 import com.fausto.bookstore.domain.Livro;
 import com.fausto.bookstore.dtos.LivroDTO;
 import com.fausto.bookstore.service.CategoriaService;
@@ -39,6 +41,18 @@ public class LivroResource {
 		List<Livro> list = livroService.findAllByCategoria(id_cat);
 		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
+		Livro newObj = livroService.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
+		Livro newObj = livroService.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
 	}
 
 }
--- src/main/java/com/fausto/bookstore/service/LivroService.java
+++ src/main/java/com/fausto/bookstore/service/LivroService.java
@@ -16,6 +16,9 @@ public class LivroService {
 	@Autowired
 	private LivroRepository repository;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -24,7 +27,22 @@ public class LivroService {
 
 
 	public List<Livro> findAllByCategoria(Integer id_cat) {
+		categoriaService.findById(id_cat);		
 		return repository.findAllByCategoria(id_cat);
+	}
+
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
 	}
 
 
