a1567c6aa1a77d92a3a6f3a78a9dbb03f7917622
faustotrindadejr@gmail.com <faustotrindadejr@gmail.com>
2021-02-14 22:08:11 -0300

    post para livro

--- src/main/java/com/fausto/bookstore/service/LivroService.java
+++ src/main/java/com/fausto/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fausto.bookstore.domain.Categoria;
 import com.fausto.bookstore.domain.Livro;
 import com.fausto.bookstore.exceptions.ObjectNotFoundException;
 import com.fausto.bookstore.repositories.LivroRepository;
@@ -43,6 +44,14 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 
 
